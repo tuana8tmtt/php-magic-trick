88107839eaca1cc8eec3850abf0d344a6d7dea22
tuana8tmt <tuana8tmt@gmail.com>
2021-03-18 15:12:23 +0700

    Finish trick4

--- trick-4.php
+++ trick-4.php
@@ -1,16 +1,13 @@
 <?php
 
 /*
- *
  * Insert Magic Code Here
- *
- *
- *
- *
- *
- *
  */
-
+$var1 = 'var';
+$
+$var1 = 'World!';
+$
+// ref :   https://www.php.net/manual/en/language.variables.variable.php
 // NO CHANGES ALLOWED BELOW
 
 $var = 'Hello!';
